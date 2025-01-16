e094f375074450d64ffce926fb3fc295f9029687
Botalov Sergey <89090168690@mail.ru>
2025-01-16 14:34:30 +0500

    added request create user

---
+++ main.py
@@ -0,0 +1,37 @@
+from faker import Faker
+import requests
+from random import choice
+
+fake = Faker()
+
+API_URL_USER = "https://backend.guider.pro/api/v2/Account/register"
+
+
+def create_fake_user():
+    char = ("!", "?", "*", ".")
+    password = fake.password(
+        length=10,
+        special_chars=True,
+        digits=True,
+        upper_case=True,
+        lower_case=True,
+    ) + choice(char)
+    data = {
+        "email": fake.email(safe=True, domain="gmail.com"),
+        "password": password,
+    }
+    print(data)
+    try:
+        response = requests.post(API_URL_USER, json=data)
+        print(response.status_code, response.json())
+    except Exception as e:
+        print(str(e))
+
+
+def main(num_requests):
+    for _ in range(num_requests):
+        create_fake_user()
+
+
+if __name__ == "__main__":
+    main(100)
